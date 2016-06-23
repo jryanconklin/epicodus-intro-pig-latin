683834a696e4d04c7bb05c4b38fe8e4acc082e1b
J. Ryan Conklin and Stephen Emery <emery.stephen@gmail.com>
2016-06-22 21:02:54 -0700

    Solve First Spec.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,48 @@
 // Business End Logic
 
 var vowels = ["a", "e", "i", "o", "u"];
-var input = "egg";
+var input = [];
+var input1;
 
 function vowelCheck() {
   vowels.forEach(function(vowel) {
     if (input.charAt(0) === vowel) {
-      input =  input + "ay";
+      input = input + "ay";
     }
   });
 }
-vowelCheck();
+
+// for (i= ) {
+//
+// }
+
+
+
+function consonantCheck() {
+  vowels.forEach(function(vowel) {
+    if (input.charAt(0) !== vowel ) {
+      // var input1 = input.slice(0, 1);
+      // console.log(input1);
+      input = input + "ay";
+    }
+  });
+}
+
+
 
 // User End Logic
+
+$(document).ready(function() {
+  $("form#pigLatinInput").submit(function(event) {
+    input = $("input#pigLatinText").val();
+
+    // if (input1 === "true") {
+    //   input = input + "ay";
+    // }
+    // if (consonantCheck() ) //This will check the function for boolean.
+    
+    consonantCheck();
+    console.log(input);
+    event.preventDefault();
+  });
+});
